0bbaff1ed88e42314681024216852b5936c8e83d
zhiyuanwang552 <zhiyuanwang552@gmail.com>
2025-09-15 12:51:30 -0400

    adding all java files

--- Polynomial.java
+++ Polynomial.java
@@ -8,17 +8,44 @@ public class Polynomial
   }
   public Polynomial(double[] given)
   {
-    coefficient = new double[given.length]
+    coefficient = new double[given.length];
     for (int i = 0; i < given.length;i++)
     {
       coefficient[i] = given[i];
     }
   }
-  public add(Polynomial given)
+  public Polynomial add(Polynomial given)
   {
-    for (int i = 0; i < given.coefficient.length; i++)
+    int max_len = Math.max(given.coefficient.length,coefficient.length);
+    double [] sum = new double[max_len];
+    for (int i = 0; i < max_len; i++)
     {
-      coefficient[i] += given.coefficient[i];
+      if (i < coefficient.length)
+      {
+        sum[i] += coefficient[i];
+      }
+      if (i < given.coefficient.length)
+      {
+        sum[i] += given.coefficient[i];
+      }
     }
-  } 
+    return new Polynomial(sum);
+  }
+  public double evaluate(double x)
+  {
+    double result = 0;
+    for (int i = 0; i < coefficient.length;i++)
+    {
+      result += coefficient[i]*(Math.pow(x,i));
+    }
+    return result;
+  }
+  public boolean hasRoot(int x)
+  {
+    if (evaluate(x) == 0)
+    {
+      return true;
+    }
+    return false;
+  }
 }
