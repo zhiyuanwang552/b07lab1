e0fe60f27b2a9108ddfd778f6f62bfcc2ec8e61f
zhiyuanwang552 <zhiyuanwang552@gmail.com>
2025-09-22 10:48:11 -0400

    second version

--- Polynomial.java
+++ Polynomial.java
@@ -2,32 +2,38 @@
 public class Polynomial
 {
   double[] coefficient;
+  double[] exponent;
   public Polynomial()
   {
     coefficient = new double[1];
-    coefficient[0] = 0;
+    exponent = new int[1]
   }
-  public Polynomial(double[] given)
+  public Polynomial(double[] given_coe, int [] given_exp)
   {
-    coefficient = new double[given.length];
-    for (int i = 0; i < given.length;i++)
+    coefficient = new double[given_coe.length];
+    exponent = new int[given_exp.length];
+    for (int i = 0; i < given_coe.length;i++)
     {
-      coefficient[i] = given[i];
+      coefficient[i] = given_coe[i];
+      exponent[i] = given_exp[i];
     }
   }
   public Polynomial add(Polynomial given)
   {
     int max_len = Math.max(given.coefficient.length,coefficient.length);
-    double [] sum = new double[max_len];
+    double [] sum_coe = new double[max_len];
+    int[] sum_exp = new int[max_len];
     for (int i = 0; i < max_len; i++)
     {
       if (i < coefficient.length)
       {
-        sum[i] += coefficient[i];
+        sum_coe[i] += coefficient[i];
+        sum_exp[i] += exponent[i];
       }
       if (i < given.coefficient.length)
       {
-        sum[i] += given.coefficient[i];
+        sum_coe[i] += given.coefficient[i];
+        sum_exp[i] += given.exponent[i];
       }
     }
     return new Polynomial(sum);
@@ -37,11 +43,11 @@ public class Polynomial
     double result = 0;
     for (int i = 0; i < coefficient.length;i++)
     {
-      result += coefficient[i]*(Math.pow(x,i));
+      result += coefficient[i]*(Math.pow(x,exponent[i]));
     }
     return result;
   }
-  public boolean hasRoot(int x)
+  public boolean hasRoot(double x)
   {
     if (evaluate(x) == 0)
     {
